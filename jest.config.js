ee0ac66c6e3fb8b7486037ab635c9680ce503f01
Victor PC <victor.palomo@fulllab.com.br>
2019-06-11 23:45:22 +0100

    Setup travis

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-  "roots": [
+  roots: [
     "<rootDir>/src"
   ],
-  "transform": {
+  transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest"
   },
+
+  testMatch: ['**/?(*.)test.js?(x)', '**/?(*.)test.ts?(x)'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,tsx,ts}',
+    '!**/node_modules/**',
+    '!**/__dumb__/**',
+    '!<rootDir>/jest.config.js',
+    '!**/coverage/**',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/'
+  ],
+  coverageReporters: ['json-summary', 'lcov'],
+  coverageDirectory: 'coverage'
 }
